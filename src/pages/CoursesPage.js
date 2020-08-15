03e202e9c43d74a33744a01502f0389f53a52cfe
Alex Lu <alu@berkeley.edu>
2020-08-15 16:16:45 -0700

    Content of courses page added

--- src/pages/CoursesPage.js
+++ src/pages/CoursesPage.js
@@ -9,7 +9,14 @@ function CoursesPage(props) {
       <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
 
       <Content>
-        <p>Hi</p>
+        <p>Replace these with small block components</p>
+        <p>CS61A: Structure and Interpretation of Computer Programs</p>
+        <p>LING100: Introduction to Linguistic Science</p>
+        <p>CS61B: Data Structures</p>
+        <p>EE16A: Designing Information Devices and Systems I</p>
+        <p>UCBUGG: 3D Modeling and Animation Decal</p>
+        <p>CS70: Discrete Mathematics and Probability Theory</p>
+        <p>CS61C: Machine Structures</p>
       </Content>
     </div>
   );
--- src/pages/ResumePage.js
+++ src/pages/ResumePage.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import Hero from "../components/Hero";
+import Content from "../components/Content";
+
 function ResumePage(props) {
-  return <p>Resume Works!</p>;
+  return (
+    <div>
+      <Hero title={props.title} />
+
+      <Content>
+        <p>Embed the pdf</p>
+      </Content>
+    </div>
+  );
 }
 
 export default ResumePage;
