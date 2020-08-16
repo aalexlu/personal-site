7125b79a502c389ee4723f0e7a5b7f6fdc256f0c
Alex Lu <alu@berkeley.edu>
2020-08-16 00:27:37 -0700

    Updated resume and built? - trying to deploy

---
+++ build/precache-manifest.cc7f75e175e2230f2b93ad7ecec05313.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61767dca3a5e830b9e181832fe3213ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e4964519851dcb626d3",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8eb81f2dbc90afef08a8",
+    "url": "/static/css/main.41652729.chunk.css"
+  },
+  {
+    "revision": "2e4964519851dcb626d3",
+    "url": "/static/js/2.71f1c705.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.71f1c705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb81f2dbc90afef08a8",
+    "url": "/static/js/main.812993a3.chunk.js"
+  },
+  {
+    "revision": "766df7dbb5392e1314ed",
+    "url": "/static/js/runtime-main.9c9c41ef.js"
+  },
+  {
+    "revision": "3ae042776b7d415d66867d3799b166dd",
+    "url": "/static/media/dosage.3ae04277.png"
+  },
+  {
+    "revision": "fe2b8126b06fb43f840ef9c31196ac48",
+    "url": "/static/media/questions.fe2b8126.png"
+  },
+  {
+    "revision": "529e3f7468d49c059f9dcecdfc077980",
+    "url": "/static/media/resume.529e3f74.png"
+  },
+  {
+    "revision": "65dd327e49a618b5e2ec12e0f5fcb3c6",
+    "url": "/static/media/website.65dd327e.png"
+  }
+]);
